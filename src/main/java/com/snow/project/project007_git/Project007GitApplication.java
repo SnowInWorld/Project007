356041076e64237adf35843bb06742ab0ba1160a
jojo <123456@snow.com>
2020-06-05 16:00:05 +0800

    首次提交

---
+++ src/main/java/com/snow/project/project007_git/Project007GitApplication.java
@@ -0,0 +1,13 @@
+package com.snow.project.project007_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project007GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project007GitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/snow/project/project007_git/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.snow.project.project007_git.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("/main")
+@RestController
+public class MainController {
+
+    @RequestMapping(value = "/home")
+    public String home(HttpServletRequest request){
+        String msg="主页";
+        return msg;
+    }
+}
